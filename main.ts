708197a74659f178dd205f41a4a1bcd32d3fdfa8
jyothircool <85424481+jyothircool@users.noreply.github.com>
2021-06-11 22:20:03 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let number = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    number = randint(1, 3)
+    if (number == 3) {
+        basic.showIcon(IconNames.Yes)
+    } else if (number == 2) {
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.showIcon(IconNames.Meh)
+    }
+})
+basic.forever(function () {
+	
+})
